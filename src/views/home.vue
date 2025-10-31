adcde86239513003f60024e6adce2a3eaa4b9de4
lixiuxian1997 <15573097235@163.com>
2025-10-31 10:41:27 +0800

    fix: 修复 fav album 页面的链接跳转问题

--- src/views/home.vue
+++ src/views/home.vue
@@ -36,7 +36,7 @@
         </div>
       </div>
     </div>
-    <div class="absolute bottom-4 right-4 text-gray-500 dark:text-white">v1.4.0</div>
+    <div class="absolute bottom-4 right-4 text-gray-500 dark:text-white">v1.4.1</div>
   </div>
 </template>
 
